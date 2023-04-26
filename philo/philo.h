415b9b84f629b9e683bb8c59a768333aa7f51f44
“gacorrei” <gguerrafranco@gmail.com>
2023-04-26 16:04:13 +0100

    Baseline

---
+++ philo/philo.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gacorrei <gacorrei@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/26 08:57:11 by gacorrei          #+#    #+#             */
+/*   Updated: 2023/04/26 09:02:56 by gacorrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <pthread.h>
+
+/*main.c*/
+
+
+#endif
